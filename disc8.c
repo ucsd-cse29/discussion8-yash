6c65cfb60027016b93a2cd8b3032b5175162961c
Yash Garde <36286221+yashgarde@users.noreply.github.com>
2024-11-22 22:10:57 +0000

    init

---
+++ disc8.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// TODO
+int bestFit(int freeBlocks[], int numBlocks, int alloc_size) {
+    
+}
+
+//Implement Best Fit
+void printBlocks(int freeBlocks[]) {
+    printf("FREE BLOCKS: ");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", freeBlocks[i]);
+    }
+    printf("\n");
+    return;
+}
+
+int main() {
+    int freeBlocks[] = {600, 100, 500, 200, 300};
+
+    int allocations[] = {232, 417, 110, 440, 80};
+    //Pre-allocations
+    printBlocks(freeBlocks);
+    for (int i = 0; i < 5; i++) {
+        printf("Allocation of size %d placed at block %d\n", allocations[i], bestFit(freeBlocks, 5, allocations[i]));
+        printBlocks(freeBlocks);
+    }
+    //POST Allocations
+    return 0;
+}
